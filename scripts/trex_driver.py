82c13328efb310c385eae772879a2cb55e15bd2f
Clemens Schuwerk <clemens.schuwerk@tum.de>
2017-07-06 10:48:29 +0200

    started to separate in to module and node

--- scripts/trex_driver.py
+++ scripts/trex_driver.py
@@ -4,11 +4,11 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64
 
-class RosConfig:
+class RosConfigPololuTrex:
 	cfg = {
-		"devices": [ "/dev/ttyUSB2" ],
+		"devices": [ "/dev/robot/ttyTrex0" ],
 		"baud": 115200,
-		"rate": 10,
+		"rate": 50,
 		"scale_i": 0.040, # 40mA per unit, according to datasheet
 		#"param_pwm_prescaler1": 0,  # 
 		#"param_pwm_prescaler2": 3,  # 
@@ -58,58 +58,50 @@ class RosConfig:
 
 
 
-class PoluluTrex:
-	is_open = False
-	have_loggederr = False
-	pub_cur = [None, None, None]
+class PololuTrex:
+    is_open = False
+    have_loggederr = False
 
-	def __init__(self, device, config, index=0):
+    def __init__(self, device, config):
 
-		self.device = device
-		self.cfg = config.cfg
-		self.dev_params = config.dev_params
-		# Publisher and subscriber
-		self.pub_cur[0] = rospy.Publisher("~current%d" % (index+0), Float64, queue_size=10)
-		self.pub_cur[1] = rospy.Publisher("~current%d" % (index+1), Float64, queue_size=10)
-		self.sub_cmd0 = rospy.Subscriber("~command%d" % (index+0), Float64, self.cb_cmd, index+0)
-		self.sub_cmd1 = rospy.Subscriber("~command%d" % (index+1), Float64, self.cb_cmd, index+1)
+        self.device = device
+        self.cfg = config.cfg
+        self.dev_params = config.dev_params
+        # Open device
+        self.dev_open()
 
-		# Open device
-		self.dev_open()
+    def dev_open(self):
+        ''' Opens and configures the TReX device '''
+        if self.is_open:
+            return True
+        try:
+            self.ser = serial.Serial(self.device, self.cfg['baud'], timeout=0.1, interCharTimeout=0.01)
+            self.is_open = True
+            self.have_loggederr = False
+            # Init
+            self.ser.write([0x81]) # CMD: get signature
+            sig = self.ser.read(9)
+            if sig[0:6] != "TReXJr":
+                rospy.logerr("Signature: %s" % sig[0:6])  
+                raise Exception("Incorrect signature")
+            self.ser.write([0x82]) # CMD: get mode
+            mode = self.ser.read(1)
+            if mode != 'a' and mode != 'r':
+                raise Exception("Incorrect mode (%c); change jumpers!" % (mode))
+            self.dev_config()
+            rospy.loginfo("Device opened: %s" % self.device)  
 
-		# Timer
-		rospy.Timer(rospy.Duration(1.0 / self.cfg['rate']), self.cb_timer)
+        except Exception as e:
+            if not self.have_loggederr:
+                rospy.logerr("Open TReX (%s) failed: %s" % (self.device,e.message))
+                #self.have_loggederr = True
+            return False
 
-	def dev_open(self):
-		''' Opens and configures the TReX device '''
-		if self.is_open:
-			return True
-		try:
-			self.ser = serial.Serial(self.device, self.cfg['baud'], timeout = 0.2)
-			self.is_open = True
-			self.have_loggederr = False
-			# Init
-			self.ser.write([0x81]) # CMD: get signature
-			sig = self.ser.read(9)
-			if sig[0:6] != "TReXJr":
-				rospy.logerr("Signature: %s" % sig[0:6])  
-				raise Exception("Incorrect signature")
-			self.ser.write([0x82]) # CMD: get mode
-			mode = self.ser.read(1)
-			if mode != 'a' and mode != 'r':
-				raise Exception("Incorrect mode (%c); change jumpers!" % (mode))
-			self.dev_config()
-			rospy.loginfo("Device opened: %s" % self.device)  
-		except Exception as e:
-			if not self.have_loggederr:
-				rospy.logerr("Open TReX failed: %s", e.message)
-				#self.have_loggederr = True
-			return False
+        rospy.loginfo("TReX device opened.")
+        return True
 
-		rospy.loginfo("TReX device opened.")
-		return True
 
-	def dev_config(self):
+    def dev_config(self):
 		''' UNFINISHED '''
 		# Go through all device parameters...
 		for name in self.dev_params.keys():
@@ -125,31 +117,98 @@ class PoluluTrex:
 				rospy.loginfo("Device Parameter %s (0x%02x)=%d: %s" % (name, param_id, val, "OK" if ok else "FAIL"))
 
 
-	def cb_timer(self, event):
-		''' Timer callback: Read current position and publish it '''
-		if not self.is_open: return
-		self.ser.write([0x8f]) # CMD: get motor currents
-		curs = self.ser.read(2)
-		if len(curs) != 2:
-			return
-		self.pub_cur[0].publish(Float64(float(ord(curs[0])) * self.cfg['scale_i']))
-		self.pub_cur[1].publish(Float64(float(ord(curs[1])) * self.cfg['scale_i']))
 
-	def cb_cmd(self, msg, index):
-		''' Callback for command message '''
-		pwm = int(round(msg.data * 0x7f))
-		if pwm < -0x7f: pwm = 0x7f
-		if pwm >  0x7f: pwm = 0x7f
-		rospy.loginfo("Received command for command%d (%s): %f=%d" % (index, self.device, msg.data, pwm))
-		# Build Command
-		cmd = 0
-		if index%2==0: cmd = 0xC0
-		if index%2 == 1: cmd = 0xC8
-		if pwm >= 0: cmd += 2  # CMD: Forward
-		if pwm < 0: cmd += 1   # CMD: Reverse
-		self.ser.write([cmd, abs(pwm)])
+    def setPWM(self, motor,data):
+        ''' Callback for command message '''
+        if not self.is_open: return          
+        pwm = int(round(data * 0x7f))
+        if pwm < -0x7f: pwm = 0x7f
+        if pwm >  0x7f: pwm = 0x7f
+
+        # Build Command
+        cmd = 0
+        if motor%2==0: cmd = 0xC0
+        if motor%2 == 1: cmd = 0xC8
+        if pwm >= 0: cmd += 2  # CMD: Forward
+        if pwm < 0: cmd += 1   # CMD: Reverse
+        self.ser.write([cmd, abs(pwm)])
+
+        
+    def getCurrents(self):
+        if not self.is_open: return    
+        try:
+            self.ser.write([0x8f]) # CMD: get motor currents
+            curs = self.ser.read(2)
+            if len(curs) != 2: return       
+            return ( float(ord(curs[0]))*self.cfg['scale_i'], float(ord(curs[1])) * self.cfg['scale_i'] )
+        
+        except Exception as e:
+           rospy.logerr("Reading currents (%s) failed: %s" % (self.device,e.message))
+
+    def getCurrent(self, motor):
+        if not self.is_open: return    
+        try:
+            self.ser.write([0x8f]) # CMD: get motor currents
+            curs = self.ser.read(2)
+            if len(curs) != 2: return       
+            return float(ord(curs[motor%2]))*self.cfg['scale_i']
+        
+        except Exception as e:
+           rospy.logerr("Reading current $d (%s) failed: %s" % (motor, self.device, e.message))
 
 
+
+class PololuTrexNode:
+    pub_cur = [None, None]
+    pwmValue = [None, None]
+    
+    def __init__(self, device, config, index=0):
+        
+        self.cfg = config.cfg
+        self.device = device
+        # Publisher and subscriber
+        self.pub_cur[0] = rospy.Publisher("~current%d" % (index+0), Float64, queue_size=10)
+        self.pub_cur[1] = rospy.Publisher("~current%d" % (index+1), Float64, queue_size=10)
+        self.sub_cmd0 = rospy.Subscriber("~command%d" % (index+0), Float64, self.cb_cmd, index+0)
+        self.sub_cmd1 = rospy.Subscriber("~command%d" % (index+1), Float64, self.cb_cmd, index+1)
+
+        # Trex Device
+        self.trex = PololuTrex(device,config)
+        
+        # Timer
+        rospy.Timer(rospy.Duration(1.0 / self.cfg['rate']), self.cb_timer)
+        
+        #self.run()
+         
+    def cb_timer(self, event):
+        
+        if self.pwmValue[0] is not None:
+                self.trex.setPWM(0,self.pwmValue[0])
+                #print "SET PWM 1 %f" % self.pwmValue[0]
+        if self.pwmValue[1] is not None:
+                self.trex.setPWM(1,self.pwmValue[1])
+        
+        currents = self.trex.getCurrent(0)
+        print currents
+        #self.pub_cur[0].publish(Float64(currents[0]))
+        #self.pub_cur[1].publish(Float64(currents[1]))
+
+        
+    def cb_cmd(self, msg, motor):
+        self.pwmValue[motor%2] = msg.data
+        rospy.loginfo("Received PWM command on command%d (%s): %f" % (motor, self.device, msg.data))
+
+        
+    def run(self):
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            if self.pwmValue[0] is not None:
+                self.trex.setPWM(0,self.pwmValue[0])
+                print "SET PWM 1 %f" % self.pwmValue[0]
+            if self.pwmValue[1] is not None:
+                self.trex.setPWM(1,self.pwmValue[1])
+            rate.sleep()
+        
 
 def shutdown_hook():
   print "shutdown time!"
@@ -157,11 +216,11 @@ def shutdown_hook():
 
 if __name__ == '__main__':
 	rospy.init_node('trex_driver', anonymous=False)
-	roscfg = RosConfig()
+	roscfg = RosConfigPololuTrex()
 	
      	i=0
 	for dev in roscfg.cfg["devices"]:
-		PoluluTrex(dev, roscfg,i)
+		PololuTrexNode(dev, roscfg,i)
 		i=i+2
 	rospy.on_shutdown(shutdown_hook)
 	rospy.spin()
